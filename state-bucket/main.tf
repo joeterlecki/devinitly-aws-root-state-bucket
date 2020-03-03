682bb96873abfdd4cbdb6cb4f7472b29564771b6
Joe Terlecki <joe.terlecki@devinitly.com>
2020-03-02 23:55:36 -0500

    push

--- state-bucket/main.tf
+++ state-bucket/main.tf
@@ -1,5 +1,5 @@
 module "devinitly-root-terraform-state" {
-  source            = "git::https://gitlab.com/devinitly/terraform/modules/terraform-aws-s3-bucket.git?ref=v1.3"
+  source            = "git::https://gitlab.com/devinitly/terraform/modules/terraform-aws-s3-bucket.git?ref=v1.3.2"
   bucket_name       = var.bucket_name
   region            = var.region
   versioning_status = var.versioning_status
