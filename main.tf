6f102d4aa8603ce7f6ba4a76c8433de4fb6d330c
Joe Terlecki <joeterlecki@pop-os.localdomain>
2020-09-27 00:02:36 -0400

    Added Logging bucket

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "devinitly-root-terraform-state" {
   source            = "github.com/joeterlecki/terraform-aws-s3-bucket.git?ref=v1.4.5"
   bucket_name       = var.bucket_name
   versioning_status = var.versioning_status
-  #logging           = var.logging
+  logging           = var.logging
   environment       = var.environment
   tags              = var.tags
 }
